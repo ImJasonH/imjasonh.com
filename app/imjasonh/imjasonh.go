ecd9d14135daba61613c1122ce4c2194a8c33f5b
Jason Hall <imjasonh@gmail.com>
2012-11-08 10:46:57 -0500

    Make app.imjasonh.com a Go app

---
+++ app/imjasonh/imjasonh.go
@@ -0,0 +1,12 @@
+package imjasonh
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Welcome to app.imjasonh.com")
+	})
+}

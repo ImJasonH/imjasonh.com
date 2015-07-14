d6e83662696f6ed0a4799f10dc2ee5f8ea1c0ab2
Jason Hall <jasonhall@google.com>
2015-07-14 10:53:27 -0400

    add liar's dice

--- update-static.sh
+++ update-static.sh
@@ -2,4 +2,6 @@ cd static/
 gsutil cp index.html gs://www.imjasonh.com/
 gsutil -h "Content-Type: text/html" cp resume gs://www.imjasonh.com/
 gsutil -h "Content-Type: text/html" cp projects gs://www.imjasonh.com/
+gsutil -h "Content-Type: text/html" cp cursor gs://www.imjasonh.com/
+gsutil -h "Content-Type: text/html" cp dice gs://www.imjasonh.com/
 cd -
